7617486c2e0d2a8d92bb9f5ffed18d74ad1e036f
JesseTheRobot <jesse.cruz.wright@gmail.com>
2025-01-26 00:23:58 +0000

    fix: compact impl for u32

--- crates/storage/codecs/derive/src/compact/mod.rs
+++ crates/storage/codecs/derive/src/compact/mod.rs
@@ -195,8 +195,8 @@ fn should_use_alt_impl(ftype: &str, segment: &syn::PathSegment) -> bool {
 pub fn get_bit_size(ftype: &str) -> u8 {
     match ftype {
         "TransactionKind" | "TxKind" | "bool" | "Option" | "Signature" | "u8" => 1,
-        "TxType" | "u32" => 2,
-        "u64" | "BlockNumber" | "TxNumber" | "ChainId" | "NumTransactions" => 4,
+        "TxType" => 2,
+        "u64" | "BlockNumber" | "TxNumber" | "ChainId" | "NumTransactions" | "u32" => 4,
         "u128" => 5,
         "U256" => 6,
         _ => 0,
